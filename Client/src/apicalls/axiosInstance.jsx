97bfe7948b06bc3ef53173d83de454441b75d19f
kuldeep8454 <maddeshiyakuldeep@gmail.com>
2023-12-11 14:46:26 +0530

    Sixth

--- Client/src/apicalls/axiosInstance.jsx
+++ Client/src/apicalls/axiosInstance.jsx
@@ -1,28 +1,28 @@
-import axios from 'axios';
+// import axios from 'axios';
 
+// export const axiosInstance = axios.create({
+//   baseURL: "http://localhost:5000"
+// });
+
+// // Add an interceptor to update headers before each request
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+ import axios from 'axios';
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000"
-});
-
-// Add an interceptor to update headers before each request
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-/* import axios from 'axios';
-export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://bargainbackend.onrender.com",
   headers: {
     authorization: `Bearer ${localStorage.getItem('token')}`
   }
 });
- */
+ 
